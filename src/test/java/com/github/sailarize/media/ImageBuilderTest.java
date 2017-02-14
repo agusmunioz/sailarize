c61271e87ed632d642058525dfab17927763ac4a
Gonzalo Testa <gonzalotesta@despegar.com>
2017-02-14 15:18:09 -0300

    Support for building images with custom paths from propeties.

---
+++ src/test/java/com/github/sailarize/media/ImageBuilderTest.java
@@ -0,0 +1,46 @@
+package com.github.sailarize.media;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Unit test for {@link ImageBuilder}
+ * 
+ * @author gonzalogtesta
+ *
+ */
+public class ImageBuilderTest {
+
+	@Test
+	public void stringImagePath() {
+		Image img = ImageBuilder.rel("test").host("server.test").path("/image/path").build();
+
+		Assert.assertEquals("Malformed image url", "server.test/image/path", img.getUrl());
+
+	}
+
+	@Test
+	public void hostFromProperties() {
+		Image img = ImageBuilder.rel("test").host("host.test").path("/image/path").build();
+
+		Assert.assertEquals("Malformed image url", "cdn.test.com/image/path", img.getUrl());
+
+	}
+
+	@Test
+	public void pathFromProperties() {
+		Image img = ImageBuilder.rel("test").host("server.test").path("paths.test").build();
+
+		Assert.assertEquals("Malformed image url", "server.test/url/path/to/image", img.getUrl());
+
+	}
+
+	@Test
+	public void fullPathFromProperties() {
+		Image img = ImageBuilder.rel("test").host("host.test").path("paths.test").build();
+
+		Assert.assertEquals("Malformed image url", "cdn.test.com/url/path/to/image", img.getUrl());
+
+	}
+}
---
+++ src/test/java/com/github/sailarize/properties/PathsTest.java
@@ -0,0 +1,35 @@
+package com.github.sailarize.properties;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link Paths}.
+ * 
+ * @author gonzalogtesta
+ *
+ */
+public class PathsTest {
+
+	/**
+	 * Test the return of a path with an existing key.
+	 */
+	@Test
+	public void existing() {
+
+		String path = Paths.get("paths.test");
+
+		Assert.assertEquals("Unexpected path from properties file.", "/url/path/to/image", path);
+	}
+
+	/**
+	 * Test a null is returned when a not existing key is used.
+	 */
+	@Test
+	public void notExisting() {
+
+		String path = Paths.get("not.exist.key");
+
+		Assert.assertNull("A path was returned with a nonexistent key.", path);
+	}
+}
